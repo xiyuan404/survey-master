c85a98e88de69d46975e66499ee2cd78f75b604d
hammondcn <bayesianifeg1@gmail.com>
2024-08-22 19:57:52 +0800

    feat(layers): 将画布中的组件以列表形式渲染到图层面板中
    同时在图层中改变选中组件会同步到画布和属性面板中

--- src/pages/survey/Edit/Layers.tsx
+++ src/pages/survey/Edit/Layers.tsx
@@ -1,6 +1,50 @@
 import React, { FC } from 'react'
+import useGetComponentInfo from 'src/hooks/useGetComponentInfo'
+
+import styles from './Layers.module.scss'
+import { Button, Space } from 'antd'
+import { EyeInvisibleOutlined, LockOutlined } from '@ant-design/icons'
+
+import classNames from 'classnames'
+import { useDispatch } from 'react-redux'
+import { changeSelectedId } from 'src/store/componentsReducer'
+
 const Layers: FC = () => {
-  return <div>layers</div>
+  const { componentList, selectedId } = useGetComponentInfo()
+
+  const dispatch = useDispatch()
+  const handleTitleClick = fe_id => {
+    if (fe_id !== selectedId) {
+      dispatch(changeSelectedId(fe_id))
+    }
+  }
+
+  return (
+    <>
+      {componentList.map(componentInfo => {
+        const { fe_id, title, isLocked, isHidden } = componentInfo
+
+        const titleDefaultClassName = styles.title
+        const selectedClassName = styles.selected
+        const titleClassName = classNames({
+          [titleDefaultClassName]: true,
+          [selectedClassName]: selectedId === fe_id,
+        })
+
+        return (
+          <div key={fe_id} className={styles['component-wrapper']}>
+            <div className={titleClassName} onClick={() => handleTitleClick(fe_id)}>
+              {title}
+            </div>
+            <Space>
+              <Button icon={<LockOutlined />} />
+              <Button icon={<EyeInvisibleOutlined />} />
+            </Space>
+          </div>
+        )
+      })}
+    </>
+  )
 }
 
 export default Layers
