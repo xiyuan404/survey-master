3eff49bef892e51d2f3a0222e831fff589ce9dc8
hammondcn <bayesianifeg1@gmail.com>
2024-08-22 23:29:57 +0800

    feat(right panel): 根据是否选中组件自动切换tab

--- src/pages/survey/Edit/rightPanel.tsx
+++ src/pages/survey/Edit/rightPanel.tsx
@@ -1,19 +1,25 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { Tabs } from 'antd'
 
 import { FileTextOutlined, SettingOutlined } from '@ant-design/icons'
 import ComponentProp from './ComponentProp'
 import PageSetting from './PageSetting'
+import useGetComponentInfo from 'src/hooks/useGetComponentInfo'
+
+enum TAB_KEYS {
+  PROP_KEY = 'prop',
+  SETTING_KEY = 'setting',
+}
 
 const tabItems = [
   {
-    key: 'prop',
+    key: TAB_KEYS.PROP_KEY,
     label: '属性',
     children: <ComponentProp />,
     icon: <FileTextOutlined />,
   },
   {
-    key: 'setting',
+    key: TAB_KEYS.SETTING_KEY,
     label: '页面设置',
     children: <PageSetting />,
     icon: <SettingOutlined />,
@@ -21,7 +27,18 @@ const tabItems = [
 ]
 
 const RightPanel: FC = () => {
-  return <Tabs defaultActiveKey="prop" items={tabItems} />
+  const [activeKey, setActiveKey] = useState(TAB_KEYS.PROP_KEY)
+  const { selectedId } = useGetComponentInfo()
+
+  useEffect(() => {
+    if (selectedId === '') {
+      setActiveKey(TAB_KEYS.SETTING_KEY)
+    } else {
+      setActiveKey(TAB_KEYS.PROP_KEY)
+    }
+  }, [selectedId])
+
+  return <Tabs activeKey={activeKey} items={tabItems} />
 }
 
 export default RightPanel
