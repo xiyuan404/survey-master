6c47e4106da62e70ec16ff4ce0211e20cafc6e2b
hammondcn <bayesianifeg1@gmail.com>
2024-08-26 12:14:36 +0800

    test: surveyInfo组件的单元测试

---
+++ src/components/SurveyComponents/SurveyInfo/Component.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+import Component from './Component'
+
+test('默认属性', () => {
+  render(<Component />) //渲染组件
+
+  const h = screen.getByText('问卷标题')
+  expect(h).toBeInTheDocument() // 断言API
+})
+
+test('传入属性', () => {
+  render(<Component title="hello" desc="world" />) //渲染组件
+
+  const h = screen.getByText('hello')
+  expect(h).toBeInTheDocument()
+
+  const p = screen.getByText('world')
+  expect(p).toBeInTheDocument()
+})
+
+test('换行显示', () => {
+  render(<Component desc={'a\n\b\nc'} />) //渲染组件
+  const span = screen.getByText('a')
+  expect(span).toBeInTheDocument()
+
+  expect(span).toHaveTextContent('a')
+  expect(span).not.toHaveTextContent('ab')
+})
